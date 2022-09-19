5b0c065cf779477f0ca68c36cb2c754c9418c42e
ANANDKUMAR <anandanuj705@gmail.com>
2022-09-19 19:07:27 +0530

    linear search algo

---
+++ linear_search.js
@@ -0,0 +1,11 @@
+// linear Search
+
+const arr = [1,2,5,7,4,9]
+
+const target = 9
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i] === target){
+        console.log(`target ${target} is on index ${i}`)
+    }
+}
